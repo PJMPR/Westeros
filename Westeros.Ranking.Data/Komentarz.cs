41a0746600dd511c591d84f6b40296cb82a205da
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2018-10-29 08:31:07 +0100

    Dodano klasę do przechowywania komentarza

---
+++ Westeros.Ranking.Data/Komentarz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Westeros.Ranking.Data
+{
+    public class Komentarz
+    {
+        private DateTime data;
+        private string tekst;
+        private string nick;
+
+        public Komentarz(string nick, string tekst, DateTime data)
+        {
+            this.data = data;
+            this.nick = nick;
+            this.tekst = tekst;
+        }
+
+        public override string ToString()
+        {
+            return data.ToString() + tekst + nick;
+        }
+    }
+}

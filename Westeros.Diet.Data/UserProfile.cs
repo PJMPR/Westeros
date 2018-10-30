4beac6b7a642751eb0bdc235569dbf4289b29a96
Miłosz Głodowski <s16620@pjwstk.edu.pl>
2018-10-30 23:00:54 +0100

    Added UserProfile class

---
+++ Westeros.Diet.Data/UserProfile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Westeros.Diet.Data
+{
+    enum Gender { Male, Female, Other}
+
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public Gender Sex { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        private List<Entry> _dairy;
+
+        public List<Entry> GetAllEntries()
+        {
+            return _dairy.ToList();
+        }
+
+        public void AddEntry(Entry entry)
+        {
+            _dairy.Add(entry);
+        }
+
+        public UserProfile(int id, Gender sex, int age, double weight, double height)
+        {
+            Id = id;
+            Sex = sex;
+            Age = age;
+            Weight = weight;
+            Height = height;
+            _dairy = new List<Entry>();
+
+        }
+    }
+}

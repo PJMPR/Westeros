4614882d6576fe9a90b0823911d3d7e153ef5d23
Mateusz Chodyna <s17606@pjwstk.edu.pl>
2018-10-30 13:12:11 +0100

    dodanie klasy diary

---
+++ Westeros.Diet.Data/Diary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Westeros.Diet.Data
+{
+
+    public class Diary
+    {
+        public int Id { get; }
+        public Entry Entry { get; }
+
+        public Diary(int id, Entry entry)
+        {
+            Id = id;
+            Entry = entry;
+
+
+        }
+    }
+}

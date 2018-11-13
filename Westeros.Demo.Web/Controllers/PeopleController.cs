3dfaafa1f6cad1aafa7a460a940a8093251a5b05
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2018-11-13 16:02:10 +0100

    add mvc demo project

---
+++ Westeros.Demo.Web/Controllers/PeopleController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Westeros.Demo.Data.Model;
+using Westeros.Demo.Data.Repositories;
+
+namespace Westeros.Demo.Web.Controllers
+{
+    public class PeopleController : Controller
+    {
+        DemoDbContext _ctx;
+
+        public PeopleController(DemoDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int id, [FromRoute] int? personId)
+        {
+            if (personId != null)
+                id = personId.Value;
+            var person = _ctx.People.FirstOrDefault(x => x.Id == id);
+            return View(person);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Person person)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _ctx.People.Add(person);
+                    _ctx.SaveChanges();
+                }
+                else return View(person);
+
+                return Redirect($"{nameof(Details)}/{person.Id}");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: People/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: People/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

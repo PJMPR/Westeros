d229836e19b2b9d49f5150249429b19bd6ab5ec8
Ventrae <s16739@pjwstk.edu.com>
2018-11-13 15:21:38 +0100

    poprawka

---
+++ Westeros.Recipes.Data/Migrations/20181106143334_devices_to_recipes.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Westeros.Recipes.Data.Migrations
+{
+    public partial class devices_to_recipes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceRecipes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeviceRecipes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DeviceID = table.Column<int>(nullable: false),
+                    RecipeID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceRecipes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_DeviceRecipes_Devices_DeviceID",
+                        column: x => x.DeviceID,
+                        principalTable: "Devices",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeviceRecipes_Recipes_RecipeID",
+                        column: x => x.RecipeID,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceRecipes_DeviceID",
+                table: "DeviceRecipes",
+                column: "DeviceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceRecipes_RecipeID",
+                table: "DeviceRecipes",
+                column: "RecipeID");
+        }
+    }
+}

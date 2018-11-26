66cb8ca4fa7baaf1d9bbad0e6320fbe602de1871
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2018-11-26 14:34:35 +0100

    add demo cotracts

--- Westeros.Demo.Web/Startup.cs
+++ Westeros.Demo.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,7 @@ namespace Westeros.Demo.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
             services.AddRepositories();
             services.AddMvc();
             var connection = Configuration.GetConnectionString("Demo");

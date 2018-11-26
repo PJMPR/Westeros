ddd1f6597c85affa3c297998a6e9c27523f126a0
DamianZarzycki <s16547@pjwstk.edu.pl>
2018-11-26 21:51:11 +0100

    Dodaje klase kalkulatora

--- Westeros.Diet.Data/Calculator.cs
+++ Westeros.Diet.Data/Calculator.cs
@@ -4,20 +4,20 @@ namespace Westeros.Diet.Data.Model
 {
     public class Calculator
     {
+       
+        public UserProfile userProfile;
 
-        private UserProfile userProfile;
+        public enum PhysicalActivity   {ExtremelyInactive, Inactive, ModeratelyActive, Active, VigorouslyActive, ExtremelyActive};
 
-        private enum PhysicalActivity   {ExtremelyInactive, Inactive, ModeratelyActive, Active, VigorouslyActive, ExtremelyActive};
+        public enum Goal  {LoseWeight, KeepWeight, GetMuscles};
 
-        private enum Goal  {LoseWeight, KeepWeight, GetMuscles};
-
-        private double BmiCalculate(UserProfile userProfile) {
+        public double BmiCalculate(UserProfile userProfile) {
            return ((userProfile.Weight) / ((userProfile.Height) * (userProfile.Height)));
         }
 
         //podstawowe zapotrzebowanie kaloryczne
 
-      private double BmrCalculate() {
+        public double BmrCalculate() {
  
             double bmr;
 
@@ -36,7 +36,7 @@ namespace Westeros.Diet.Data.Model
             }
         }
 
-        private double TotalCaloricDemand(PhysicalActivity physicalActivity, Goal goal)
+        public double TotalCaloricDemand(PhysicalActivity physicalActivity, Goal goal)
         {
            double totalBmr = BmrCalculate();
             
--- Westeros.Diet.Tests/UnitTest1.cs
+++ Westeros.Diet.Tests/UnitTest1.cs
@@ -1,13 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
 
 namespace Westeros.Diet.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        Calculator obj = new Calculator();
+       
         [TestMethod]
-        public void TestMethod1()
+        public void BmiCalculateTest()
         {
+           return true;
+        }
+
+        public void BmrCalculateTest()
+        {
+            return true;
+        }
+
+        public void TotalCaloricDemandTest()
+        {
+            return true;
         }
     }
 }

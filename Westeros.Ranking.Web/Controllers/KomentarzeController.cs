f5307ad915f11cc285bd78926fe231d62f3fc8ed
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2018-11-27 14:49:44 +0100

    Wyświetalnie komentarzy dodane

---
+++ Westeros.Ranking.Web/Controllers/KomentarzeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Westeros.Ranking.Data.Repositories;
+
+namespace Westeros.Ranking.Web.Controllers
+{
+    public class KomentarzeController : Controller
+    {
+        private readonly StarkDbContext _context;
+        public KomentarzeController(StarkDbContext c)
+        {
+            _context = c;
+        }
+
+        public IActionResult View()
+        {
+            return View(_context.Komentarz.ToList());
+        }
+    }
+}

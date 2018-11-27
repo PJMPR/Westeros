f6e3d14a9d5adc887694fc0aa7b0425c6b50b67a
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2018-11-27 16:23:00 +0100

    add broken unit tests

--- Westeros.Demo.Tests/UnitTest1.cs
+++ Westeros.Demo.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Westeros.Demo.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(false);
+            Assert.IsTrue(true);
         }
     }
 }

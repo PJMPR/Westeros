f5dcdae00ea50e53b68871e18f05c27e5edf9896
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2018-11-27 15:22:06 +0100

    fix build. add travis config

--- Westeros.Diet.Tests/UnitTest1.cs
+++ Westeros.Diet.Tests/UnitTest1.cs
@@ -1,27 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Calculator;
 
 namespace Westeros.Diet.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        Calculator obj = new Calculator();
-       
-        [TestMethod]
-        public void BmiCalculateTest()
-        {
-           return true;
-        }
-
-        public void BmrCalculateTest()
-        {
-            return true;
-        }
-
-        public void TotalCaloricDemandTest()
-        {
-            return true;
-        }
     }
 }

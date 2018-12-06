45cbdb76dd7c62cf99cd6c3dc54ea383856a3330
Maciej Milewski <s17947@pjwstk.edu.pl>
2018-12-06 12:37:09 +0100

    Pliki migrazcji bazy danych.

---
+++ Westeros.UserProfile.Data/Migrations/20181205211725_CreateUserDB2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Westeros.UserProfile.Data.Migrations
+{
+    public partial class CreateUserDB2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "weight",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<double>(
+                name: "height",
+                table: "User",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "height",
+                table: "User");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "weight",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}

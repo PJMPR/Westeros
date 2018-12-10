d408f3ed1b69bd6f7afce6863a7ba512adeccbf7
s17472 <s17472@LAB111W-16>
2018-12-10 14:13:28 +0100

    add

---
+++ Westeros.Diet.Data/Model/Diet.cs
@@ -0,0 +1,12 @@
+﻿namespace Westeros.Diet.Data.Model
+{
+    public class Diet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; private set; }
+        public decimal Proteins { get; private set; }
+        public decimal Carbohydrates { get; private set; }
+        public decimal Fats { get; private set; }
+    }
+}

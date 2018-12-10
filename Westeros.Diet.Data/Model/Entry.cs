1809457d0ee3cb4db31245ade27d61f158d51cb5
Ben <s17472@pjwstk.edu.pl>
2018-12-10 19:07:15 +0100

    fix

--- Westeros.Diet.Data/Model/Entry.cs
+++ Westeros.Diet.Data/Model/Entry.cs
@@ -10,7 +10,6 @@ namespace Westeros.Diet.Data.Model
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Weight { get; set; }
-        //private ICollection<IIngredient> _ingredients ;
         public ICollection<IngredientEntry> EntryIngredients { get; set; }
         public ICollection<RecipeEntry> EntryRecipes { get; set; }
         public int UserProfileId { get; set; }

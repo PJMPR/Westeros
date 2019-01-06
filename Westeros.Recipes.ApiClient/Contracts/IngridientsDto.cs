3a7254c9e9dc83f15b7645b8daf3c1467047322a
kulaszewicz <36889025+kulaszewicz@users.noreply.github.com>
2019-01-06 19:35:37 +0100

    Api dev work

---
+++ Westeros.Recipes.ApiClient/Contracts/IngridientsDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Westeros.Demo.ApiClient.Contracts
+{
+    public class IngridientsDto
+    {
+        public int Id { get; set; } 
+        public int IngridientId { get; set; }
+        public string Name { get; set; } 
+        public enum CategoryType { Vegetable, Fruit, Meat, Fish, Dairy, Delicacies, Other };
+        public CategoryType Category { get; set; } 
+        public double Proteins { get; set; } 
+        public double Fats { get; set; } 
+        public double Carbohydrates { get; set; } 
+        public double Calories { get; set; } 
+        public string PhotoPath { get; set; } 
+        public double AvgPrice { get; set; } 
+    }
+}
---
+++ Westeros.Recipes.ApiClient/RecipesApiClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+using Westeros.Demo.ApiClient.Contracts;
+using Microsoft.Extensions.Configuration;
+
+namespace Westeros.Demo.ApiClient
+{
+    public class RecipesApiClient : IRecipesApiClient
+    {
+        HttpClient _httpClient;
+
+        public RecipesApiClient(string baseAddress)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(baseAddress);
+        }
+
+
+
+        public async void AddNewPerson(IngridientsDto personToAdd)
+        {
+            await _httpClient.PostAsJsonAsync("api/People", personToAdd);
+        }
+        public async void UpdatePerson(IngridientsDto personToUpdate)
+        {
+            await _httpClient.PutAsJsonAsync("api/People", personToUpdate);
+        }
+        public IEnumerable<IngridientsDto> All()
+        {
+            return _httpClient
+                .GetAsync("api/People")
+                .Result
+                .Content
+                .ReadAsAsync<IEnumerable<IngridientsDto>>()
+                .Result;
+        }
+        public IngridientsDto GetById(int id)
+        {
+            return _httpClient
+                       .GetAsync($"api/People/{id}")
+                       .Result
+                       .Content
+                       .ReadAsAsync<IngridientsDto>()
+                       .Result;
+        }
+
+        public async void DeletePerson(int id)
+        {
+            await _httpClient.DeleteAsync($"api/People/{id}");
+        }
+
+    }
+}

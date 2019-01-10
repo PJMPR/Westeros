55ec2643b939f67390482119daa3e9f107779f18
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-01-10 20:00:25 +0100

    Added Annotations to model

--- Westeros.Ranking.Data/Migrations/20190110185820_StarkDataBase.Designer.cs
+++ Westeros.Ranking.Data/Migrations/20190110185820_StarkDataBase.Designer.cs
@@ -11,9 +11,10 @@ using Westeros.Ranking.Data.Repositories;
 namespace Westeros.Ranking.Data.Migrations
 {
     [DbContext(typeof(StarkDbContext))]
-    partial class StarkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190110185820_StarkDataBase")]
+    partial class StarkDataBase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Westeros.Ranking.Data/Model/Dieta.cs
+++ Westeros.Ranking.Data/Model/Dieta.cs
@@ -1,10 +1,15 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Westeros.Ranking.Data.Model
 {
     public class Dieta
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public int DietaId { get; set; }
+        [Range(0,double.MaxValue)]
         public int IloscOdwiedzin { get; set; }
     }
 }

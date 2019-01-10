e62b675e7d34c111698daf31183f8db773bd3c36
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-01-10 22:16:02 +0100

    Added Html that can be iframed into other websites

--- Westeros.Ranking.Web/Controllers/OcenaController.cs
+++ Westeros.Ranking.Web/Controllers/OcenaController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 using Westeros.Ranking.Data.Model;
 using Westeros.Ranking.Data.Repositories;
+using Westeros.Ranking.Web.Models;
 
 namespace Westeros.Ranking.Web.Controllers
 {
@@ -25,22 +28,29 @@ namespace Westeros.Ranking.Web.Controllers
 
         public IActionResult AddOcena()
         {
-            return View(new Oceny());
+            return View(new AddOcenaClass());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddOcena(Oceny ocena)
+        public ActionResult AddOcena(AddOcenaClass a)
         {
+            ArrayList Attributes = new ArrayList(a.UrlElements.Substring(1).Split('='));
+            Oceny newOcena=new Oceny();
+            newOcena.Ocena = a.Ocena;
+            newOcena.Data = DateTime.Now;
+            newOcena.Nick = a.Nick;
+            newOcena.Tekst = a.Tekst;
+            
             try
             {
                 if (ModelState.IsValid)
                 {
-                    _context.Oceny.Add(ocena);
+                    _context.Oceny.Add(newOcena);
                     _context.SaveChanges();
                 }
                 else
-                    return View(ocena);
+                return View(a);
 
                 return RedirectToAction(nameof(View));
             }
---
+++ Westeros.Ranking.Web/Models/AddOcenaClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Westeros.Ranking.Web.Models
+{
+    public class AddOcenaClass
+    {
+        [Required]
+        public string Nick { get; set; }
+        [Required]
+        public string Tekst { get; set; }
+        [Required]
+        [Range(1,5)]
+        public int Ocena { get; set; }
+        public string UrlElements { get; set; }
+    }
+}

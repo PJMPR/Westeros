9afaa7b1c0fb112dfab690ce64b3f48970f85a76
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-01-10 20:01:22 +0100

    Added a basic ocena adder

--- Westeros.Ranking.Web/Controllers/OcenaController.cs
+++ Westeros.Ranking.Web/Controllers/OcenaController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using Westeros.Ranking.Data.Model;
 using Westeros.Ranking.Data.Repositories;
 
 namespace Westeros.Ranking.Web.Controllers
@@ -18,6 +21,33 @@ namespace Westeros.Ranking.Web.Controllers
         public IActionResult View()
         {
             return View(_context.Oceny.ToArray());
+        }
+
+        public IActionResult AddOcena()
+        {
+            return View(new Oceny());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddOcena(Oceny ocena)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Oceny.Add(ocena);
+                    _context.SaveChanges();
+                }
+                else
+                    return View(ocena);
+
+                return RedirectToAction(nameof(View));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(View));
+            }
         }
     }
 }

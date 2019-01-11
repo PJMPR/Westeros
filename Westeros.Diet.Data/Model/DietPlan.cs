a020301f27f7ebaee236414057f8065072b4dfbd
Ben <s17472@pjwstk.edu.pl>
2019-01-11 01:46:55 +0100

    fiz

--- Westeros.Diet.Data/Model/DietPlan.cs
+++ Westeros.Diet.Data/Model/DietPlan.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Calories { get; private set; }
-        public decimal Proteins { get; private set; }
-        public decimal Carbohydrates { get; private set; }
-        public decimal Fats { get; private set; }
+        public int Calories { get; set; }
+        public decimal Proteins { get; set; }
+        public decimal Carbohydrates { get; set; }
+        public decimal Fats { get; set; }
         public UserProfile UserProfile { get; set; }
     }
 }

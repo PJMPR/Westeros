7d3db8ecf35547a32716f7f25a1aa51013422e9d
kulaszewicz <36889025+kulaszewicz@users.noreply.github.com>
2019-01-13 23:29:18 +0100

    Controllers changes

---
+++ Westeros.Recipes.Web/Models/Buffor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Westeros.Recipes.Data.Model;
+
+namespace Westeros.Recipes.Web.Models
+{
+    public class Buffor
+    {
+        public List<Ingredient> Ingredients = new List<Ingredient>();
+
+        public List<Device> Devices = new List<Device>();
+    }
+}

d09a6a20980cff9cdbcae34fcaa99b7076de4db3
Ben <s17472@pjwstk.edu.pl>
2019-01-14 05:43:13 +0100

    fix

--- Westeros.Diet.Service/Program.cs
+++ Westeros.Diet.Service/Program.cs
@@ -16,45 +16,6 @@ namespace Westeros.Diet.Service
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
 
-                var en = new Entry
-                {
-                    Date = DateTime.Now,
-                    UserProfileId = 1
-                };
-
-                context.Entries.Add(en);
-                context.SaveChanges();
-
-                ICollection<EntryIngredient> ing = new List<EntryIngredient>();
-                ICollection<EntryRecipe> rec = new List<EntryRecipe>();
-
-                foreach (var item in context.Recipes.ToList())
-                {
-                    rec.Add(new EntryRecipe
-                    {
-                        RecipeId = item.Id,
-                        Entry = en,
-                        EntryId = en.Id
-                    });
-                }
-
-                context.EntryRecipes.Add(rec);
-                context.SaveChanges();
-
-                foreach (var item in context.Ingredients.Where(i => i.Id > 2))
-                {
-                    ing.Add(new EntryIngredient
-                    {
-                        IngredientId = item.Id,
-                        IngredientQuantity = 2,
-                        Entry = en,
-                        EntryId = en.Id
-                    });
-                }
-
-                context.Add(ing);
-                context.SaveChanges();
-
                 Console.WriteLine("Hello World!");
                 Console.ReadKey();
             }

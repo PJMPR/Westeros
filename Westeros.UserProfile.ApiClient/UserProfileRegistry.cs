13b9f0f742ab6d8e6556d7e89903951b79d3831f
15465 <40267367+15465@users.noreply.github.com>
2019-01-19 23:54:40 +0100

    API RESTOWE JEJ

--- Westeros.UserProfile.ApiClient/UserProfileRegistry.cs
+++ Westeros.UserProfile.ApiClient/UserProfileRegistry.cs
@@ -6,11 +6,11 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class UserProfileApiRegistry
     {
-        /*
+        
        public static IServiceCollection AddUserProfileClient(this IServiceCollection services, IConfiguration config)
        {
 
           return services.AddTransient<IUserProfileApiClient>(s => new UserProfileApiClient(config.GetSection("UserProfileApi").Value));
-        }*/
+        }
     }
 }

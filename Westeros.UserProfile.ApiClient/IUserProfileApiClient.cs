f1405a13ffbab9c2a38c66ed3b075ee65e5c15d3
15465 <40267367+15465@users.noreply.github.com>
2019-01-19 23:02:04 +0100

    Szablon na rest API

---
+++ Westeros.UserProfile.ApiClient/IUserProfileApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Westeros.UserProfile.ApiClient.Contracts;
+
+namespace Westeros.UserProfile.ApiClient
+{
+    public interface IUserProfileApiClient
+    {
+        void AddUser(UserDto userToAdd);
+        IEnumerable<UserDto> All();
+        void DeleteUser(int id);
+        UserDto GetById(int id);
+        void UpdatePerson(UserDto userToUpdate);
+    }
+}
---
+++ Westeros.UserProfile.ApiClient/UserProfileRegistry.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Westeros.UserProfile.ApiClient;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class UserProfileApiRegistry
+    {
+        /*
+       public static IServiceCollection AddUserProfileClient(this IServiceCollection services, IConfiguration config)
+       {
+
+          return services.AddTransient<IUserProfileApiClient>(s => new UserProfileApiClient(config.GetSection("UserProfileApi").Value));
+        }*/
+    }
+}

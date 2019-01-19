c094e888b1d39988cd5d8bc75de7ad1152733176
15465 <40267367+15465@users.noreply.github.com>
2019-01-19 20:51:20 +0100

    Migracja

--- Westeros.UserProfile.Data/Migrations/20190119194708_CreateUserDB8.Designer.cs
+++ Westeros.UserProfile.Data/Migrations/20190119194708_CreateUserDB8.Designer.cs
@@ -12,9 +12,10 @@ using Westeros.UserProfile.Data.Repositories;
 namespace Westeros.UserProfile.Data.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190119194708_CreateUserDB8")]
+    partial class CreateUserDB8
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Westeros.UserProfile.Data/Migrations/20190119194708_CreateUserDB8.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Westeros.UserProfile.Data.Migrations
+{
+    public partial class CreateUserDB8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "weight",
+                table: "User",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "gender",
+                table: "User",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "age",
+                table: "User",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "height",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "height",
+                table: "User");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "weight",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "gender",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "age",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}

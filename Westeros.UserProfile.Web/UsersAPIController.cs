50aed0c6301797e1fa9f4e7a8a86fcc56f5bff95
Maciej Milewski <s17947@pjwstk.edu.pl>
2019-01-19 21:23:31 +0100

    Create UsersAPIController.cs

---
+++ Westeros.UserProfile.Web/UsersAPIController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Westeros.UserProfile.Data;
+using Westeros.UserProfile.Data.Repositories;
+
+namespace Westeros.UserProfile.Web
+{
+    [Produces("application/json")]
+    [Route("api/UsersAPI")]
+    public class UsersAPIController : Controller
+    {
+        private readonly UserDbContext _context;
+
+        public UsersAPIController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UsersAPI
+        [HttpGet]
+        public IEnumerable<User> GetUser()
+        {
+            return _context.User;
+        }
+
+        // GET: api/UsersAPI/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.User.SingleOrDefaultAsync(m => m.id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/UsersAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser([FromRoute] int id, [FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UsersAPI
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.id }, user);
+        }
+
+        // DELETE: api/UsersAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.User.SingleOrDefaultAsync(m => m.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.id == id);
+        }
+    }
+}

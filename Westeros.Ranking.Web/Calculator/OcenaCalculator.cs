e0fd69bbb044640e22e5a818bb57a3185fb5cf49
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-01-20 20:58:16 +0100

    Dodane sortery i kalkulatory do ocen

---
+++ Westeros.Ranking.Web/Calculator/OcenaCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Westeros.Ranking.Data.Model;
+
+namespace Westeros.Ranking.Web.Calculator
+{
+    public class OcenaCalculator
+    {
+
+        public IList<Oceny> OcenyDoPrzepisu(Oceny[] o, int id)
+        {
+            IList<Oceny> ret=new List<Oceny>();
+            foreach (Oceny ocena in o)
+            {
+                if (ocena.id==id)
+                {
+                    ret.Add(ocena);
+                }
+            }
+
+            return ret;
+        }
+
+        public IList<Oceny> ocenyRosnaco(Oceny[] o)
+        {
+            IList<Oceny>ret=new List<Oceny>(o);
+            ret.OrderBy(x => x.Ocena).ToList();
+            return ret;
+        }
+
+        public IList<Oceny> ocenymalejaco(Oceny[] o)
+        {
+            IList<Oceny>ret=new List<Oceny>(o);
+            ret.OrderByDescending(x => x.Ocena);
+            return ret;
+        }
+
+    }
+}

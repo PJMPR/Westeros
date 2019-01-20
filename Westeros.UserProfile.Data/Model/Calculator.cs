46875a285b86143ad0c3e2e56c1fc563d7f038e6
Maciej Milewski <s17947@pjwstk.edu.pl>
2019-01-20 21:23:08 +0100

    Kalkulator

---
+++ Westeros.UserProfile.Data/Model/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Westeros.UserProfile.Data.Model
+{
+    public class Calculator
+    {
+        public decimal BMI (User user)
+        {
+            return (user.weight / (user.height * user.height) ?? 0);
+        }
+
+        // dane z formularza
+        public decimal WHR (decimal hip, decimal waist)
+        {
+            return (waist/hip);
+        }
+
+        public decimal BMR_Harrisa_Benedicta(User user)
+        {
+            decimal w = user.weight ?? 0;
+            decimal h = user.height ?? 0;
+
+            if (user.gender == Gender.Male)
+            {
+                return (66 + ((decimal)13.7 * w) + (5 * h) - (decimal)(6.76 * user.age));
+            }
+            else
+            {
+                return (655 + ((decimal)9.6 * w) + ((decimal)1.8 *h) - (decimal)(6.76 * user.age));
+            }
+        }
+
+        public decimal BMR_Mifflin_StJeor(User user)
+        {
+            decimal w = user.weight ?? 0;
+            decimal h = user.height ?? 0;
+
+            if (user.gender == Gender.Male)
+            {
+                return (((decimal)9.99 * w) + ((decimal)6.25 * h) - (decimal)(4.92 * user.age) + 5);
+            }
+            else
+            {
+                return (((decimal)9.99 * w) + ((decimal)6.25 * h) - (decimal)(4.92 * user.age) - 161);
+            }
+        }
+
+        // dane z formularza
+        public decimal AMR (int bm, int m, int u, int d, int bd)
+        {
+            return (bm*(decimal)1.4 + m*(decimal)2.5 + u*(decimal)4.2 + d*(decimal)8.2 + bd*12);
+        }
+
+        // Jeżeli użytkownik poda dane do BMR i AMR, to można podać indeks TER, który jest ich sumą
+        public decimal TER (User user, int bm, int m, int u, int d, int bd)
+        {
+            return (BMR_Harrisa_Benedicta(user) + AMR(bm,m,u,d,bd));
+        }
+    }
+}
--- Westeros.UserProfile.Tests/UnitTest1.cs
+++ Westeros.UserProfile.Tests/UnitTest1.cs
@@ -1,13 +1,54 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Westeros.UserProfile.Data;
+using Westeros.UserProfile.Data.Model;
 
 namespace Westeros.UserProfile.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        /*[TestMethod]
+        public void BMI_Test()
         {
+            Calculator c = new Calculator();
+
+            User user = new User();
+            user.height = 1.87;
+            user.weight = 93;
+
+            var weight = user.weight;
+            var height = user.height;
+
+            decimal uWeight = (decimal)weight;
+            decimal uHeight = (decimal)height;
+            decimal expected = 26.5949841287998m;
+            
+            var amount = Calculator.BmiCalculate(user);
+            decimal actual = (decimal)amount;
+
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void BmrCalculateTest()
+        {
+            UserProfile userProfile = new UserProfile();
+            userProfile.Age = 25;
+            userProfile.Height = 1.87;
+            userProfile.Weight = 93;
+            userProfile.Sex = Sex.Male;
+
+            var userWeight = userProfile.Weight;
+            var userHeight = userProfile.Height;
+
+            decimal uWeight = (decimal)userWeight;
+            decimal uHeight = (decimal)userHeight;
+            decimal expected = 1979.82m;
+
+            var amount = Calculator.BmrCalculate(userProfile);
+            decimal actual = (decimal)amount;
+
+            Assert.AreEqual(expected, actual);
+        }*/
     }
 }

715ac2f99148284c21820b3c9cf96ee9a0ada97c
Maciej Milewski <s17947@pjwstk.edu.pl>
2019-01-20 22:20:41 +0100

    Update ICalculator.cs

--- Westeros.UserProfile.Data/Services/ICalculator.cs
+++ Westeros.UserProfile.Data/Services/ICalculator.cs
@@ -6,6 +6,11 @@ namespace Westeros.UserProfile.Data.Services
 {
     public interface ICalculator
     {
-        int execute(int x);
+        decimal BMI(User user);
+        decimal WHR(decimal hip, decimal waist);
+        decimal BMR_Harrisa_Benedicta(User user);
+        decimal BMR_Mifflin_StJeor(User user);
+        decimal AMR(int bm, int m, int u, int d, int bd);
+        decimal TER(User user, int bm, int m, int u, int d, int bd);
     }
 }

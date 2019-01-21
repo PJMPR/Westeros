958b28dc53e95b309cb0ca1595da95afbfa76506
Maciej Milewski <s17947@pjwstk.edu.pl>
2019-01-21 12:15:36 +0100

    Create UsersRecipes.sql

---
+++ Westeros.UserProfile.Data/UsersRecipes.sql
@@ -0,0 +1,12 @@
+﻿insert into dbo.UserRecipe 
+(UserID, RecipeID)
+values
+(1,3);
+insert into dbo.UserRecipe 
+(UserID, RecipeID)
+values
+(1,1);
+insert into dbo.UserRecipe 
+(UserID, RecipeID)
+values
+(5,5);

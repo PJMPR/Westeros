aece16624564f042a84441819db8a7551bda193b
Jeremi Rycharski <s17538@pjwstk.edu.pl>
2019-01-21 01:16:24 +0100

    Inserty userów

---
+++ Westeros.UserProfile.Data/Users.sql
@@ -0,0 +1,490 @@
+INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Jeremi',  -- name - nvarchar(max)
+    N'Rycharski',  -- surname - nvarchar(max)
+    N'Mozezdam',  -- login - nvarchar(max)
+    N'sesjatokoszmar@gmail.com',  -- email - nvarchar(max)
+    23,  -- age - int
+    79, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    183  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Izabela',  -- name - nvarchar(max)
+    N'Bak',  -- surname - nvarchar(max)
+    N'Baku156',  -- login - nvarchar(max)
+    N'iza2321@onet.pl',  -- email - nvarchar(max)
+    20,  -- age - int
+    69, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    180  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Pawel',  -- name - nvarchar(max)
+    N'Tawerniany',  -- surname - nvarchar(max)
+    N'Pawelek111',  -- login - nvarchar(max)
+    N'Tawerna9999@wp.pl',  -- email - nvarchar(max)
+    40,  -- age - int
+    90, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    199  -- height - decimal(18, 2)
+    );
+INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Donald',  -- name - nvarchar(max)
+    N'Tusk',  -- surname - nvarchar(max)
+    N'PSL1',  -- login - nvarchar(max)
+    N'polityczniepoprawny@onet.pl',  -- email - nvarchar(max)
+    49,  -- age - int
+    89, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    190  -- height - decimal(18, 2)
+    );
+INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Karolina',  -- name - nvarchar(max)
+    N'Mejdej',  -- surname - nvarchar(max)
+    N'KM0',  -- login - nvarchar(max)
+    N'karola13222@onet.pl',  -- email - nvarchar(max)
+    32,  -- age - int
+    49, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    177  -- height - decimal(18, 2)
+    );
+INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Kinga',  -- name - nvarchar(max)
+    N'Rusin',  -- surname - nvarchar(max)
+    N'Rus8585',  -- login - nvarchar(max)
+    N'Rus8585@o2.pl',  -- email - nvarchar(max)
+    29,  -- age - int
+    66, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    177  -- height - decimal(18, 2)
+    );
+INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Olga',  -- name - nvarchar(max)
+    N'Krzyzowa',  -- surname - nvarchar(max)
+    N'Krzyzu2332',  -- login - nvarchar(max)
+    N'cross999@o2.pl',  -- email - nvarchar(max)
+    24,  -- age - int
+    59, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    18  -- height - decimal(18, 2)
+    );
+
+INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Jan',  -- name - nvarchar(max)
+    N'Kowalski',  -- surname - nvarchar(max)
+    N'kowal',  -- login - nvarchar(max)
+    N'kowal@onet.pl',  -- email - nvarchar(max)
+    18,  -- age - int
+    69, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    190  -- height - decimal(18, 2)
+    );
+
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Marta',  -- name - nvarchar(max)
+    N'Nopowska',  -- surname - nvarchar(max)
+    N'nop',  -- login - nvarchar(max)
+    N'martanop@onet.pl',  -- email - nvarchar(max)
+    20,  -- age - int
+    50, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    150  -- height - decimal(18, 2)
+    );
+
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Iwona',  -- name - nvarchar(max)
+    N'Janowska',  -- surname - nvarchar(max)
+    N'Iwonka',  -- login - nvarchar(max)
+    N'lol123@onet.pl',  -- email - nvarchar(max)
+    24,  -- age - int
+    80, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    169  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Mateusz',  -- name - nvarchar(max)
+    N'Bomba',  -- surname - nvarchar(max)
+    N'hehe1',  -- login - nvarchar(max)
+    N'bomba@onet.pl',  -- email - nvarchar(max)
+    65,  -- age - int
+    100, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    160  -- height - decimal(18, 2)
+    );
+
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Wojtek',  -- name - nvarchar(max)
+    N'Jan',  -- surname - nvarchar(max)
+    N'Wojak',  -- login - nvarchar(max)
+    N'wojtek2321@wp.pl',  -- email - nvarchar(max)
+    25,  -- age - int
+    67, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    155  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Michal',  -- name - nvarchar(max)
+    N'Trojanowski',  -- surname - nvarchar(max)
+    N'trojan',  -- login - nvarchar(max)
+    N'trojan1@interia.pl',  -- email - nvarchar(max)
+    25,  -- age - int
+    100, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    198  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Aleksandra',  -- name - nvarchar(max)
+    N'Dzas',  -- surname - nvarchar(max)
+    N'olcia',  -- login - nvarchar(max)
+    N'wow12333@gmail.pl',  -- email - nvarchar(max)
+    30,  -- age - int
+    50, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    144  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Mateusz',  -- name - nvarchar(max)
+    N'Janocha',  -- surname - nvarchar(max)
+    N'janiemoge',  -- login - nvarchar(max)
+    N'ajednakmoge@wp.pl',  -- email - nvarchar(max)
+    23,  -- age - int
+    100, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    180  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Elzbieta',  -- name - nvarchar(max)
+    N'Makler',  -- surname - nvarchar(max)
+    N'wallstreet',  -- login - nvarchar(max)
+    N'bank@gmail.com',  -- email - nvarchar(max)
+    40,  -- age - int
+    66, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    172  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Bartosz',  -- name - nvarchar(max)
+    N'Blaszczykowski',  -- surname - nvarchar(max)
+    N'Pucio',  -- login - nvarchar(max)
+    N'pucio6@wp.pl',  -- email - nvarchar(max)
+    22,  -- age - int
+    77, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    166  -- height - decimal(18, 2)
+    );
+	
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Mateusz',  -- name - nvarchar(max)
+    N'Rus',  -- surname - nvarchar(max)
+    N'chasmer',  -- login - nvarchar(max)
+    N'chasmer@hentai.com',  -- email - nvarchar(max)
+    21,  -- age - int
+    75, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    188  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Waldemar',  -- name - nvarchar(max)
+    N'Jelop',  -- surname - nvarchar(max)
+    N'Glupek1',  -- login - nvarchar(max)
+    N'madry321321@onet.pl',  -- email - nvarchar(max)
+    50,  -- age - int
+    110, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    188  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Nikola',  -- name - nvarchar(max)
+    N'Browarek',  -- surname - nvarchar(max)
+    N'Nikola1',  -- login - nvarchar(max)
+    N'piwko420@interia.pl',  -- email - nvarchar(max)
+    15,  -- age - int
+    45, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    144  -- height - decimal(18, 2)
+    );
+		INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Nikodem',  -- name - nvarchar(max)
+    N'Zywy',  -- surname - nvarchar(max)
+    N'Imdead',  -- login - nvarchar(max)
+    N'ocochdozi@lol.pl',  -- email - nvarchar(max)
+    29,  -- age - int
+    99, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    199  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Piotr',  -- name - nvarchar(max)
+    N'Wasak',  -- surname - nvarchar(max)
+    N'Wasiu79',  -- login - nvarchar(max)
+    N'wasal@interia.pl',  -- email - nvarchar(max)
+    28,  -- age - int
+    88, -- weight - decimal(18, 2)
+    0,    -- gender - int
+    191  -- height - decimal(18, 2)
+    );
+	INSERT INTO dbo.[User]
+(
+    name,
+    surname,
+    login,
+    email,
+    age,
+    weight,
+    gender,
+    height
+)
+VALUES
+(   N'Matylda',  -- name - nvarchar(max)
+    N'Rozwon',  -- surname - nvarchar(max)
+    N'Madzia22',  -- login - nvarchar(max)
+    N'Matyldarzondzi@onet.pl',  -- email - nvarchar(max)
+    15,  -- age - int
+    45, -- weight - decimal(18, 2)
+    1,    -- gender - int
+    144  -- height - decimal(18, 2)
+    );
+
+

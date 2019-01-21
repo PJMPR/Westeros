d02aab73ce68cdb22e68f64bde60c9b64937fd08
Jeremi Rycharski <s17538@pjwstk.edu.pl>
2019-01-21 01:06:35 +0100

    Inserty przepisów

---
+++ Westeros.UserProfile.Data/Recipes.sql
@@ -0,0 +1,70 @@
+INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Spaghetti Bolognese'  -- name - nvarchar(max)
+   
+    );
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Kurczak faszerowany'  -- name - nvarchar(max)
+   
+    );
+	
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Sandacz w sosie cebulowym'  -- name - nvarchar(max)
+   
+    );
+	
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Pizza wegetarianska'  -- name - nvarchar(max)
+   
+    );
+	
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Pulpety w sosie wlasnym'  -- name - nvarchar(max)
+   
+    );
+	
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Indyk na slodko'  -- name - nvarchar(max)
+   
+    );
+	
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Gulasz z kalmarow'  -- name - nvarchar(max)
+   
+    );
+	
+	INSERT INTO dbo.[Recipe]
+(
+    name
+)
+VALUES
+(   'Salatka cezarska'  -- name - nvarchar(max)
+   
+    );
